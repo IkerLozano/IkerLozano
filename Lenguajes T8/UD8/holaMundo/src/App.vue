ab3b012f86a49fae3e569fd916d61ea71d8cca1c
ikerlozano <ilozanor@iesch.org>
2026-04-16 19:54:39 +0200

    subo el T8

---
+++ Lenguajes T8/UD8/holaMundo/src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+    const nombre = "Vue 3"
+     const arrayFrutas = ["🍎", "🍌", "🍉", "🍓", "🍒"];
+     const descragaFrutas = [
+        {
+            name: "Manzana",
+            price: "1.00€",
+            descripcion: "Manzana del himalaia",
+            stock: 0
+        },
+         {
+            name: "Pera",
+            price: "2.50€",
+            descripcion: "Pera de Pascual",
+            stock: 67
+        },
+         {
+            name: "Naranja",
+            price: "3.00€",
+            descripcion: "Naranja de DAM1",
+            stock: 8
+        }
+     ]
+
+     const fruta = 
+        {
+            name: "Fresa",
+            price: "4.00€",
+            descripcion: "Fresa"
+        }
+    
+</script>
+
+<template>
+    <h1>Hola {{ nombre }}</h1>
+    <hr>
+    <h3>Directiva v-for</h3>
+
+    <p>Recorremos un array</p>
+    <p>{{ arrayFrutas }}</p>
+    <ul class="sinPunto">
+        <li v-for="(fruta, index) in arrayFrutas" :key="index">
+            {{ index }} - {{ fruta}}
+        </li>
+
+    </ul>
+    
+    <hr>
+    <p>Recorremos un array de Objetos</p>
+    <ul>
+        <li v-for="(fruta) in descragaFrutas" :Key="fruta.name"> <!--la key es para poner como clave primaria el nombre-->
+            {{ fruta.name }} - {{ fruta.descripcion }} -- {{ fruta.price }}
+        </li>
+    </ul>
+    <hr>
+    <p>Recorremos un objeto</p>
+    <ul>
+        <li v-for ="(valor, propiedad, index) in fruta">
+           {{ propiedad }} - {{ valor }} - {{ index }}
+        </li>
+    </ul>
+</template>
+
+<style>
+    .sinPunto{
+        list-style-type: none;
+    }
+</style>
---
+++ Lenguajes T8/UD8/holaMundo/src/App2.vue
@@ -0,0 +1,39 @@
+<script setup>
+    const nombre = " La clase de DAM1"
+    const styleColor = "color:blue;"
+    const active = false
+    const colores = ["blue", "red", "green"]
+</script>
+
+
+<template>
+    <h1>Hola mundo{{ nombre }}</h1>
+    <hr>
+    <h2 v-bind:style="styleColor">Hola {{ nombre }}</h2>
+    <h2 :style="styleColor">Hola {{ nombre.toUpperCase() }}</h2>
+    <hr>
+    <p>{{ active ? 'Estoy activo' : 'Estoy inactivo' }}</p>
+    <p>{{ colores }}</p>
+    <h3 :style="`color: ${colores[0]}`">Color con interpolacion</h3>
+    <hr>
+
+    <!--Directiva v-if-->
+    <div v-if ="active === true"><h2>Actice vale true</h2></div>
+    <p v-else-if = "active === false">Actice vale false</p>
+    <div v-else ="active === false"><h2>no se sabe lo que vale active</h2></div>
+
+    <!--Directiva v-show, si no quieres q se vea la va a esconder -->
+    <hr>
+    <h3>Directiva v-show</h3>
+    <h2 v-show="active">Active vale true</h2>
+
+    
+</template>
+
+<style>
+    h1{
+        color: red;
+    }
+</style>
+
+
