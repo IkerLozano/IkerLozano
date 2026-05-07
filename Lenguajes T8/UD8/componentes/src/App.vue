b8b3f6775e9bfd935bbea1892c3296e67e03ea31
ikerlozano <ilozanor@iesch.org>
2026-05-07 18:02:28 +0200

    dia 7

---
+++ Lenguajes T8/UD8/componentes/src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+import BotonContador from './components/BotonContador.vue';
+import BlogPost from './components/BlogPost.vue';
+import { ref } from 'vue';
+
+
+const posts = ref ([
+  {id: 1, title: "Posts 1", body: "Contenido Posts 1"},
+  {id: 2, title: "Posts 2", body: "Contenido Posts 2"},
+  {id: 3, title: "Posts 3", body: "Contenido Posts 3"},
+  {id: 4, title: "Posts 4"},
+  {id: 5, title: "Posts 5", body: "Contenido Posts 5"},
+])
+
+
+const miFavorito = ref("")
+
+const cambiarFavorito = (titulo) => {
+  miFavorito.value = titulo;
+}
+
+</script>
+
+
+<template>
+
+  <div class="container">
+    <h1>DAM 1 - 2026</h1>
+
+    <BotonContador />
+
+    <hr>
+
+    <h2>Mi post favorito: {{ miFavorito}}</h2>
+
+    <div class="container">
+    <BlogPost 
+      v-for="post in posts"
+        :key="post.id"
+      :title=post.title 
+      :body=post.body
+      :id=post.id
+      colorText="primary"
+      @cambiarTituloFavorito = "cambiarFavorito"
+      class="mb-2"
+      />
+
+    </div>
+    
+
+    
+
+
+  </div>
+
+
+</template>
+
+
+<style scoped></style>
---
+++ Lenguajes T8/UD8/componentes/src/components/BlogPost.vue
@@ -0,0 +1,39 @@
+<script setup>
+import BotonContador from './BotonContador.vue';
+
+
+    //defineProps(['title', 'body', 'id', 'colorText'])
+
+    const props = defineProps({
+        title: String,
+        id: Number,
+        body: {
+            type: String,
+            default: 'Sin descripcion'
+        }
+    })
+
+    
+</script>
+
+
+<template>
+
+    <div class="card">
+        <div class="card-body">
+            <div class="card-title">
+                {{ id }} - {{title}}
+            </div>
+            <p>
+                {{ body }}
+            </p>
+            <button 
+                @click="$emit('cambiarTituloFavorito', title)"
+                class="btn btn-outline-primary">
+                    Mi favorito
+            </button>
+        </div>
+    </div>
+
+
+</template>
