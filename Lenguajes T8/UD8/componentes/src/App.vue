6f3daec0a7588a4685265a9a4d68b4a7be867b0b
ikerlozano <ilozanor@iesch.org>
2026-05-14 18:25:44 +0200

    dia 14-componentes

--- Lenguajes T8/UD8/componentes/src/App.vue
+++ Lenguajes T8/UD8/componentes/src/App.vue
@@ -1,24 +1,55 @@
 <script setup>
 import BotonContador from './components/BotonContador.vue';
 import BlogPost from './components/BlogPost.vue';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
+import PaginatePosts from './components/PaginatePosts.vue';
+import LoadingSpinner from './components/LoadingSpinner.vue';
 
 
-const posts = ref ([
-  {id: 1, title: "Posts 1", body: "Contenido Posts 1"},
-  {id: 2, title: "Posts 2", body: "Contenido Posts 2"},
-  {id: 3, title: "Posts 3", body: "Contenido Posts 3"},
-  {id: 4, title: "Posts 4"},
-  {id: 5, title: "Posts 5", body: "Contenido Posts 5"},
-])
+const posts = ref ([])
 
 
 const miFavorito = ref("")
+
+
+//paginacion
+const postXpagina = 10;
+const inicio = ref(0)
+const fin = ref(postXpagina)
+
+//loading
+const loading = ref(true)
+
+
+//metodos para la paginacion
+const next = () => {
+  inicio.value = inicio.value + postXpagina
+  fin.value = fin.value + postXpagina
+}
+
+const previous = () => {
+  inicio.value = inicio.value - postXpagina
+  fin.value = fin.value - postXpagina
+}
 
 const cambiarFavorito = (titulo) => {
   miFavorito.value = titulo;
 }
 
+
+
+//vamos a usar fetch para cargar datos deuna API publica
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json())
+  .then(data => posts.value = data)
+  .finally(
+    setTimeout(() => {
+      loading.value = false
+    }, 2000)
+  
+  )
+
+const tamanoPost = computed(() => posts.value.length) //100
 </script>
 
 
@@ -32,11 +63,22 @@ const cambiarFavorito = (titulo) => {
     <hr>
 
     <h2>Mi post favorito: {{ miFavorito}}</h2>
+    <LoadingSpinner v-if="loading"/>
 
-    <div class="container">
+    <div class="container" v-else>
+     
+    <PaginatePosts 
+      class="mb-2"
+      @next="next"
+      @previous="previous"
+      :inicio="inicio"
+      :fin="fin"
+      :tamanio="tamanoPost"
+      />
+
     <BlogPost 
-      v-for="post in posts"
-        :key="post.id"
+      v-for="post in posts.slice(inicio, fin)"
+      :key="post.id"
       :title=post.title 
       :body=post.body
       :id=post.id
--- Lenguajes T8/UD8/componentes/src/components/BlogPost.vue
+++ Lenguajes T8/UD8/componentes/src/components/BlogPost.vue
@@ -2,19 +2,23 @@
 import BotonContador from './BotonContador.vue';
 
 
-    //defineProps(['title', 'body', 'id', 'colorText'])
+//defineProps(['title', 'body', 'id', 'colorText'])
 
-    const props = defineProps({
-        title: String,
-        id: Number,
-        body: {
-            type: String,
-            default: 'Sin descripcion'
-        }
-    })
+const props = defineProps({
+    title: String,
+    id: Number,
+    body: {
+        type: String,
+        default: 'Sin descripcion'
+    }
+})
 
-    
+
 </script>
+
+
+
+
 
 
 <template>
@@ -22,15 +26,13 @@ import BotonContador from './BotonContador.vue';
     <div class="card">
         <div class="card-body">
             <div class="card-title">
-                {{ id }} - {{title}}
+                {{ id }} - {{ title }}
             </div>
             <p>
                 {{ body }}
             </p>
-            <button 
-                @click="$emit('cambiarTituloFavorito', title)"
-                class="btn btn-outline-primary">
-                    Mi favorito
+            <button @click="$emit('cambiarTituloFavorito', title)" class="btn btn-outline-primary">
+                Mi favorito
             </button>
         </div>
     </div>
